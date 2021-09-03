8f87a059930f5c16768b5b620ad4d230055b0aa8
Julian Bolanos <julian.camilob24@gmail.com>
2021-09-03 18:46:25 -0500

    Generic class Nodo implemented

--- src/collections/Nodo.java
+++ src/collections/Nodo.java
@@ -1,5 +1,31 @@
 package collections;
 
-public class Nodo {
-
+public class Nodo<T> {
+	
+	private T t;
+	private Nodo<T> izquierdo;
+	private Nodo<T> derecho;
+	
+	Nodo(T t){
+		this.t = t;
+		derecho = null;
+		izquierdo = null;	
+	}
+	
+	private void cambiarValor(T t) {
+		this.t = t;
+	}
+	
+	private T mostrarValor() {
+		return t;
+	}
+	
+	private Nodo<T> darIzquierdo() {
+		return izquierdo;
+	}
+	
+	private Nodo<T> darDerecho() {
+		return derecho;
+	}
+	
 }
