21c85760af29ff7bfb030b310cebb5f14a8a62bf
lyca22 <ariep2206@gmail.com>
2021-09-03 19:25:39 -0500

    Implemented uncomplete ABB methods

--- src/collections/ABB.java
+++ src/collections/ABB.java
@@ -1,5 +1,31 @@
 package collections;
 
-public class ABB {
+public class ABB<N> {
+
+	private N r;
+	
+	public ABB(N r) {
+		this.setR(r);
+	}
+	
+	public N getR() {
+		return r;
+	}
+
+	public void setR(N r) {
+		this.r = r;
+	}
+	
+	public void añadirNodo(N n) {
+		
+	}
+
+	public void eliminarNodo(N n) {
+		
+	}
+
+	public <T> N buscarNodo(T t) {
+		return null;
+	}
 
 }
